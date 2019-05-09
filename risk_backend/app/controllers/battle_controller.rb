bcfc2f95a7aee5d76f9ca8eb0fdb8e917595f657
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-05-09 21:34:23 +0100

    battle lock catches put in

--- risk_backend/app/controllers/battle_controller.rb
+++ risk_backend/app/controllers/battle_controller.rb
@@ -4,19 +4,21 @@ class GameController < ApplicationController
 
   def attack
     if params[:user_id] == Game.STATE[:currentPlayer].id
-      Battle.attack(params[:base_territory_id], params[:target_territory_id], params[:armies])
-      render json: Game.gameState, status: :accepted
-    else
-      render json: {error: 'Not your turn'}, status: 400
-    end
+      case Battle.attack(params[:base_territory_id], params[:target_territory_id], params[:armies])
+      when 0
+        render json: Game.gameState, status: :accepted
+      when 1
+        render json: {error: 'Cannot Attack From There'}, status: 400
+      end
   end
 
   def continue_attack
     if params[:user_id] == Game.STATE[:currentPlayer].id
       case Battle.continueAttack(params[:continue_attack])
       when 0
-      render json: Game.gameState, status: :accepted
+        render json: Game.gameState, status: :accepted
       when 1
+        render json: {error: 'Cannot Attack From There'}, status: 400
       end
     else
       render json: {error: 'Not your turn'}, status: 400
