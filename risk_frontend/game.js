712a54458ad8c9e92b1cf87c524ead66625490e0
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-05-10 13:13:24 +0100

    player session storage added

--- risk_frontend/game.js
+++ risk_frontend/game.js
@@ -1,6 +1,11 @@
 class Game {
-
   static state = {}
+  static player = sessionStorage.getItem("player")
 
-
+  static connect = user => {
+    const name = user.name;
+    const colour = user.colour;
+    return API.ConnectServer(name, colour)
+      .then(resp => sessionStorage.setItem(resp));
+  };
 }
