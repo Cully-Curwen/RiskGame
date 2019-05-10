9fa8f65447dd65b014eda42844cf3a2eeb34a2ef
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-05-10 12:01:13 +0100

    battle testing

--- risk_backend/app/controllers/battle_controller.rb
+++ risk_backend/app/controllers/battle_controller.rb
@@ -17,12 +17,8 @@ class BattleController < ApplicationController
 
   def continue_attack
     if params[:user_id] == Game.STATE[:currentPlayer].id
-      case Battle.continueAttack(params[:continue_attack])
-      when 0
+      Battle.continueAttack(params[:continue_attack])
         render json: {gameState: Game.gameState, battleLock: Battle.LOCK}.to_json, status: :accepted
-      when 1
-        render json: {error: 'Cannot Attack From There'}, status: 400
-      end
     else
       render json: {error: 'Not your turn'}, status: 400
     end
